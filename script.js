fb356ed4f106204e536307bed97b8ff91b490727
Safayat Hossain <safayathossain40@gmail.com>
2024-07-25 21:33:21 +0600

    added

---
+++ script.js
@@ -0,0 +1,20 @@
+//player 1
+var randomNumber1 = Math.floor((Math.random()*6)+1);
+var randomImage1 ="./images/dice"+randomNumber1+".png";
+document.querySelectorAll("img")[0].setAttribute("src",randomImage1);
+
+
+//player 2
+var randomNumber2 = Math.floor((Math.random()*6)+1);
+var randomImage2 ="./images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomImage2);
+
+
+//setting up who wins
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "player 1 wins!!";
+}else if( randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "player 2 wins !!";
+}else{
+    document.querySelector("h1").innerHTML = "Draw!!";
+}
